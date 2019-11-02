c6080ab98169238880ff2e9d7e95a60915395a38
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-02 01:56:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,4 @@ def update_counting_hash(hash, key)
    hash[key] = 1
  end
  return hash 
-  # rif the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
 end
